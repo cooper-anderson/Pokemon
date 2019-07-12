ad0f22542a3b10a0f8419bf7be04d10882523787
Cooper Anderson <cooper@cooperstuff.ninja>
2019-07-11 19:34:59 -0700

    Reorganize file structure

--- src/main/java/ninja/cooperstuff/pokemon/client/Launch.java
+++ src/main/java/ninja/cooperstuff/pokemon/client/Launch.java
@@ -2,10 +2,6 @@ package ninja.cooperstuff.pokemon.client;
 
 import ninja.cooperstuff.debug.Debug;
 import ninja.cooperstuff.engine.Game;
-import ninja.cooperstuff.engine.events.KeyListener;
-import ninja.cooperstuff.engine.events.Keys;
-import ninja.cooperstuff.pokemon.init.Moves;
-import ninja.cooperstuff.pokemon.type.Type;
 
 public class Launch {
 	private static Game game = new Game();
@@ -20,12 +16,8 @@ public class Launch {
 		ninja.cooperstuff.pokemon.init.Init.postInit();
 
 		while (game.running) {
-			//if (KeyListener.isKeyPressed(Keys.SPACE)) game.instantiate(new TestObject());
 			game.update();
 			game.repaint();
-
-			//System.out.print(Moves.bubble.points);
-			//System.out.println("\n");
 			Thread.sleep((long) (1000.0 / 60.0));
 		}
 	}
