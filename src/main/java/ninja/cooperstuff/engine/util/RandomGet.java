e39b7ae0936e1e075944d4edb31552214add4f03
Cooper Anderson <cooper@cooperstuff.ninja>
2019-07-31 23:10:08 -0700

    Some util for random

---
+++ src/main/java/ninja/cooperstuff/engine/util/RandomGet.java
@@ -0,0 +1,16 @@
+package ninja.cooperstuff.engine.util;
+
+import java.util.Collection;
+import java.util.Random;
+
+public class RandomGet {
+	public static <T> T get(Collection<T> collection) {
+		int index = new Random().nextInt(collection.size());
+		int i = 0;
+		for (T item : collection) {
+			if (i == index) return item;
+			i++;
+		}
+		return null;
+	}
+}
---
+++ src/main/java/ninja/cooperstuff/pokemon/util/RandomWeight.java
@@ -0,0 +1,48 @@
+package ninja.cooperstuff.pokemon.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class RandomWeight<T> {
+	private ArrayList<WeightContainer> items = new ArrayList<>();
+	private HashMap<T, Integer> itemsMap = new HashMap<>();
+	private int size = 0;
+
+	public RandomWeight<T> add(T item, int weight) {
+		this.size += weight;
+		if (itemsMap.containsKey(item)) {
+			this.size -= this.items.get(this.itemsMap.get(item)).weight;
+			this.items.get(this.itemsMap.get(item)).weight = weight;
+		}
+		else {
+			int index = this.items.size();
+			this.items.add(new WeightContainer(item, weight));
+			this.itemsMap.put(item, index);
+		}
+		return this;
+	}
+
+	public T get(int index) {
+		int current = 0;
+		for (WeightContainer container : this.items) {
+			if (index < current + container.weight) return container.item;
+			current += container.weight;
+		}
+		return null;
+	}
+
+	public T get() {
+		return this.get(new Random().nextInt(this.size));
+	}
+
+	private class WeightContainer {
+		T item;
+		int weight;
+
+		WeightContainer(T item, int weight) {
+			this.item = item;
+			this.weight = weight;
+		}
+	}
+}
